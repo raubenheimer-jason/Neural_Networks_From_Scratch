f8c543d357e8820421248bad262bdc59fec7350a
Jason Raubenheimer <raubenheimer.jason@gmail.com>
2022-09-30 10:35:30 +0100

    Add project files.

---
+++ Neural_Networks_From_Scratch/Neural_Networks_From_Scratch.cpp
@@ -0,0 +1,9 @@
+// Neural_Networks_From_Scratch.cpp
+// From the YouTube series: https://www.youtube.com/playlist?list=PLQVvvaa0QuDcjD5BAw2DxE6OF2tius3V3
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
